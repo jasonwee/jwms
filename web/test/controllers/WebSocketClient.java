7ef23b557174580c3a2e4ba10c30841a5d099e06
Jason Wee <peichieh@gmail.com>
2017-01-05 00:02:37 +0800

    added logginlistener static class

--- web/test/controllers/WebSocketClient.java
+++ web/test/controllers/WebSocketClient.java
@@ -8,6 +8,8 @@ import org.asynchttpclient.ListenableFuture;
 import org.asynchttpclient.ws.WebSocket;
 import org.asynchttpclient.ws.WebSocketListener;
 import org.asynchttpclient.ws.WebSocketUpgradeHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WebSocketClient {
 	
@@ -24,6 +26,35 @@ public class WebSocketClient {
 		final ListenableFuture<WebSocket> future = requestBuilder.execute(handler);
 		final CompletableFuture<WebSocket> completableFuture = future.toCompletableFuture();
 		return completableFuture;
+	}
+	
+	static class LoggingListener implements WebSocketListener {
+		
+		private Logger logger = LoggerFactory.getLogger(LoggingListener.class);
+		
+		private Throwable throwableFound = null;
+		
+		public Throwable getThrowable() {
+			return throwableFound;
+		}
+
+		@Override
+		public void onOpen(WebSocket websocket) {
+			logger.info("On open: ");
+			websocket.sendMessage("hello");
+		}
+
+		@Override
+		public void onClose(WebSocket websocket) {
+			logger.info("onClose: ");
+		}
+
+		@Override
+		public void onError(Throwable t) {
+			logger.error("onError: ", t);
+			throwableFound = t;
+		}
+		
 	}
 
 }
